4c6b66d0323a4ab6678be92d0c1348d38927ec00
Armo <115414955+darkarmo@users.noreply.github.com>
2024-08-02 16:26:57 +0545

    tested list methods

--- listtest.py
+++ listtest.py
@@ -1,4 +1,4 @@
-x=[]
+'''x=[]
 for i in range(0,5):
     y=input('Enter the name of student')
     x.append(y)
@@ -6,3 +6,17 @@ print("List of students",x[0:5])
 for i in x:
     if (i[0]=="A" or i[0]=="a"):
         print(i)
+'''
+'''
+#inserting
+y=[1,2,3]
+index=int(input("Enter index"))
+data=input("Enter data")
+y.insert(index,data)
+print(y)
+'''
+#remove
+y=[1,"hari",3]
+data=int(input("Enter data"))
+y.remove(data)
+print(y)
