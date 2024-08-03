abeab0cd066b028eb6df16835731227a00ce4a53
Armo <115414955+darkarmo@users.noreply.github.com>
2024-08-03 14:23:06 +0545

    while loop test

--- while loop.py
+++ while loop.py
@@ -7,9 +7,18 @@ while (count<=10):
 #program to ask user name of student and store it in a list and ask user until to select Y or N
 name=[]
 n="y"
-while (n=="y" or n=="Y"):#(n!="N" or n!=="n")
+#while (n=="y" or n=="Y"):#
+'''while(n!="N"):
     stu=input("Write the name of the student")
     name.append(stu)
     n=input("Do you want to add more (Y for yes , N for no )")
+
+print(name)
+'''
+stu="Demo"
+#until last character in A or a
+while(stu[-1]!="a"):
+    stu=input("Write the name of the student")
+    name.append(stu)
 
 print(name)
