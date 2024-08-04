ef213ac4e9789d886f6775276eb540db3ff501c9
Armo <115414955+darkarmo@users.noreply.github.com>
2024-08-04 16:19:23 +0545

    class and objects

--- class.py
+++ class.py
@@ -1,15 +1,25 @@
 class student():
     name="Hyu"
     age="22"
-    def __init__(self,name,age):
+    def __init__(self,name=None,age=None): #constructor
         print(f"I am called {self.name}")
         self.name=name
         self.age=age
+    def intro(self):
+        print(f"My name is {self.name} and I am {self.age} year old.")
 
 stu=student("Harry",23)
 stu1=student("Karin",45)
 print(type(stu))
+
 print(stu.name)
 print(stu.age)
 print(stu1.name)
 print(stu1.age)
+stu.intro()
+stu1.intro()
+stu2=student()
+
+stu2.name="Ramesh"
+stu2.age=23
+stu2.intro()
---
+++ classtest.py
@@ -0,0 +1,7 @@
+class test():
+    name="Demo"
+    def print(self):
+        print(f"{self.name}")
+
+t=test()
+t.print()
