f84342bcbc54ce15d522e59b1f28741e052e54a1
Armo <115414955+darkarmo@users.noreply.github.com>
2024-08-04 13:41:33 +0545

    dictionary

--- arbitary argument.py
+++ arbitary argument.py
@@ -6,7 +6,15 @@ def add(*args):
     return sum
 
 print(add(3,4,5,6,7,8,9,10))
+#default values
 def greet(firstname,lastname):
     print(f"Hello,{firstname}{lastname}")
 greet("John","Doe")
 greet(lastname="Doe",firstname="John")
+
+def display_person(firstname=None,middlename=None,lastname=None):
+    if middlename==None:
+        print(f"{firstname} {lastname}!")
+    else:
+        print(f"{firstname} {middlename} {lastname}!")
+display_person(firstname="John",lastname="Doe")
---
+++ dictionary.py
@@ -0,0 +1,27 @@
+#key mapping
+student={
+    'name':"Harry",
+    'age':22,
+    'school':"DAV School"
+}
+student['gender']="Male"
+print(f"The students name is {student['name']} and age is {student['age']}")
+print(student.get("name"))
+print(student.get("gender"))
+print(student.get("Hollow"))
+#print(student['hollow']) (ERROR)
+
+#program to take name,age,gender of 3 students from user and print it
+number=3
+stu=[]
+for i in range(3):
+    name=input("Enter the name")
+    age=int(input("Enter the age"))
+    gender=input("Enter the gender")
+    student= {
+        "name":name,
+        "age":age,
+        "gender":gender
+    }
+    stu.append(student)
+print(stu)
